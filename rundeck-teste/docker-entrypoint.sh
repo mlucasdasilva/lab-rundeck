388604ad3b181784fc22218942b55788118a442c
Root GSAP <gsap-apps@bndes.gov.br>
2019-06-07 16:35:21 -0300

    inclusao de configurações

--- rundeck-teste/docker-entrypoint.sh
+++ rundeck-teste/docker-entrypoint.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
+#
+#
 echo "Rotina de preprocessamento"
-# Pre-processing steps
-
 echo
+#
+# Pre-processing steps:
+
+
+ssh-keygen -A  && /usr/sbin/sshd
+
+export JAVA_HOME=$(readlink -f $(which java))
+
+#
+#
 echo "Iniciando aplicação"
 echo
 echo "Command:"
 echo $@
+
+
 exec $@
 
